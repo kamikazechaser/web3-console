38a96aaab71dff73ff200655600076b1a925172d
imollov <ivomollov@gmail.com>
2020-09-20 14:25:58 +0300

    Remove deprecated calls

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-import repl from 'repl';
-import paraseArgs from 'minimist';
-import chalk from 'chalk';
-import Web3 from 'web3';
+import repl from "repl";
+import paraseArgs from "minimist";
+import chalk from "chalk";
+import Web3 from "web3";
 
 const args = paraseArgs(process.argv.slice(2));
 const arg = (args._ || [])[0];
 
-let url = 'http://localhost:8545';
+let url = "http://localhost:8545";
 
-if (typeof arg === 'string') {
+if (typeof arg === "string") {
   url = arg;
-} else if (typeof arg === 'number') {
+} else if (typeof arg === "number") {
   url = `http://localhost:${arg}`;
 }
 
@@ -24,10 +24,8 @@ function prettyInfo(name, value) {
 
 try {
   process.stdout.write(`
-${prettyInfo('RPC Endpoint', url)}
-${prettyInfo('Node Version', web3.version.node)}
-${prettyInfo('Latest Block', web3.eth.blockNumber)}
-${prettyInfo('Network ID #', web3.version.network)}
+${prettyInfo("RPC Endpoint", url)}
+${prettyInfo("Web3 Version", web3.version)}
 
 `);
   global.web3 = web3;
